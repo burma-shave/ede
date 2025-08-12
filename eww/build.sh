4445765d3dfa494fb3d03d46175ea5f1c6786e42
Eric Jutrzenka <eric@jutr.me>
2025-08-12 11:55:44 -0700

    Remove commented out code

--- eww/build.sh
+++ eww/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# check niri already installed
-# check curl already installed
-
 sudo apt-get install -y gcc clang libgtk-3-dev libdbusmenu-glib-dev libdbusmenu-gtk3-dev libgtk-layer-shell-dev libpango1.0-dev
 
 sudo snap install rustup --classic
@@ -11,13 +8,5 @@ rustup default stable
 git clone https://github.com/elkowar/eww
 cd eww
 cargo build --release --no-default-features --features=wayland
-#
-# cd ~
-# git clone https://github.com/YaLTeR/niri
-# cd niri
-# git checkout 25.05.1
-# cargo build --release
-#
-# cp -r resources /niri/
-# cp target/release/niri /niri/
+
 
